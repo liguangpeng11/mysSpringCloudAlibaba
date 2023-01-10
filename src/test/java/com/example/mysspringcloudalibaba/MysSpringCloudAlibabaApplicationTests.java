25664f742b370bbe2bdc6ab80505985caab68ddb
liguangpeng11 <1250034275@qq.com>
2023-01-10 20:51:41 +0800

    123

---
+++ src/test/java/com/example/mysspringcloudalibaba/MysSpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysspringcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysSpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
